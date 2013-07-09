5c70eba57964ff7334260880d11e4a2d1d6037bc
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-07-09 14:54:53 +0000

    V1.1 --->  V1.2 Release

---  display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++  display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -105,7 +105,7 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(29, 13)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "V1.1"
+        Me.Label6.Text = "V1.2"
         '
         'ComboBox1
         '
