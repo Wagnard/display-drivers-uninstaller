d70f7a733c38ecab5b5efd1e6a994ccb7620ee95
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-07-16 00:49:41 +0000

    More HydraVision Stuff. Version bump to 1.6.3 in preparation to release.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -106,9 +106,9 @@ Partial Class Form1
         Me.Label6.AutoSize = True
         Me.Label6.Location = New System.Drawing.Point(13, 292)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(29, 13)
+        Me.Label6.Size = New System.Drawing.Size(38, 13)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "V1.6"
+        Me.Label6.Text = "V1.6.3"
         '
         'ComboBox1
         '
