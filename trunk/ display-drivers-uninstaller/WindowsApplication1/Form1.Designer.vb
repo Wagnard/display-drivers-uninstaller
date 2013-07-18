2f8190ffb19dbdff4c060113db3c10886fa22b90
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-07-18 19:55:10 +0000

    Release 2.3 -AMD
    Fixed a readonly file that prevented the deletion of the SteadyVideo folder.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -108,7 +108,7 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(29, 13)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "V2.2"
+        Me.Label6.Text = "V2.3"
         '
         'ComboBox1
         '
