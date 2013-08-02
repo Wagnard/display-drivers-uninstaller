973b0240d7d0e6932f03513b43e2e7366b23275d
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-08-02 20:22:47 +0000

    Reverted the threading until we fix the log. Bumping version to 3.8 as preparation to a release.
    NVIDIA
    
    Also remove the 3D vision USB Driver (this will fix som oem that does not get erased for some users.

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.5.0.0")> 
-<Assembly: AssemblyFileVersion("3.5.0.0")> 
+<Assembly: AssemblyVersion("3.8.0.0")> 
+<Assembly: AssemblyFileVersion("3.8.0.0")> 
