433240dbffe24cfc45058193a81c6551703c24c5
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-08-07 20:32:15 +0000

    -Bumping version to 3.9 for future release. -Added a restart of the display driver "devcon restart =display" to possibly help blackscreen issue. (I still think its driver fault tho.) -Some misc stuff like thread sleep reduction.

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.8.0.0")> 
-<Assembly: AssemblyFileVersion("3.8.0.0")> 
+<Assembly: AssemblyVersion("3.9.0.0")> 
+<Assembly: AssemblyFileVersion("3.9.0.0")> 
