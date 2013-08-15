f189d187733998ea0fe97e8543d6d63120896b7f
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-08-15 18:45:21 +0000

    Preparation for 5.0 release
    -Uninstall with devcon hidden device too.
    -fixed other possible null exception crash

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.3.3.0")> 
-<Assembly: AssemblyFileVersion("4.3.3.0")> 
+<Assembly: AssemblyVersion("5.0.0.0")> 
+<Assembly: AssemblyFileVersion("5.0.0.0")> 
