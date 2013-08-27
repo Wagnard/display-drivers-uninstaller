c35353908e4db8267022c9af113d94d471ea685e
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-08-27 19:07:15 +0000

    V6.1 NVIDIA updated the pnplockdown files with more regkeys to be fix to be cleaned. (Could solve problem with some people installing some driver and or windows update driver.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -119,7 +119,7 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(29, 13)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "V6.0"
+        Me.Label6.Text = "V6.1"
         '
         'ComboBox1
         '
