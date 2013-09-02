208c3df9c66e8841dd01f67b4a4b483099de0d04
Shady7557@gmail.com <Shady7557@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-09-02 22:34:33 +0000

    Not labelled as 6.3 and missing some code from Wagnard.
    (untested by me.)

---  display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++  display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -42,6 +42,10 @@ Partial Class Form1
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.VisitGuru3dNVIDIAThreadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VisitGuru3dAMDThreadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VisitGeforceThreadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.MenuStrip1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -49,7 +53,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(210, 92)
+        Me.Button1.Location = New System.Drawing.Point(8, 168)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(195, 70)
         Me.Button1.TabIndex = 0
@@ -60,16 +64,16 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 28)
+        Me.Label1.Location = New System.Drawing.Point(5, 27)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(145, 13)
+        Me.Label1.Size = New System.Drawing.Size(142, 13)
         Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Detected Windows Version : "
+        Me.Label1.Text = "Detected Windows Version: "
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(164, 27)
+        Me.Label2.Location = New System.Drawing.Point(153, 25)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(10, 13)
         Me.Label2.TabIndex = 2
@@ -78,7 +82,7 @@ Partial Class Form1
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(164, 41)
+        Me.Label3.Location = New System.Drawing.Point(153, 38)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(10, 13)
         Me.Label3.TabIndex = 3
@@ -88,36 +92,35 @@ Partial Class Form1
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(93, 206)
+        Me.Label4.Location = New System.Drawing.Point(6, 465)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(401, 26)
+        Me.Label4.Size = New System.Drawing.Size(344, 13)
         Me.Label4.TabIndex = 4
-        Me.Label4.Text = "Note : AMD / NVIDIA has nothing to do with this application. If something go wron" & _
-    "g, " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "it is your responsability. This program contains code from Microsoft WDK 8." & _
-    ""
+        Me.Label4.Text = "Note: DDU and it's creators are in no way affiliated with AMD or NVIDIA."
         '
         'TextBox1
         '
-        Me.TextBox1.Location = New System.Drawing.Point(53, 258)
+        Me.TextBox1.Location = New System.Drawing.Point(39, 320)
         Me.TextBox1.Multiline = True
         Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
         Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(526, 134)
+        Me.TextBox1.Size = New System.Drawing.Size(379, 116)
         Me.TextBox1.TabIndex = 6
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(6, 258)
+        Me.Label5.Location = New System.Drawing.Point(5, 350)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.Size = New System.Drawing.Size(28, 13)
         Me.Label5.TabIndex = 7
-        Me.Label5.Text = "Log :"
+        Me.Label5.Text = "Log:"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(6, 379)
+        Me.Label6.Location = New System.Drawing.Point(6, 491)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(29, 13)
         Me.Label6.TabIndex = 8
@@ -129,7 +132,7 @@ Partial Class Form1
         Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"NVIDIA", "AMD"})
-        Me.ComboBox1.Location = New System.Drawing.Point(167, 57)
+        Me.ComboBox1.Location = New System.Drawing.Point(122, 54)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 9
@@ -137,18 +140,18 @@ Partial Class Form1
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(12, 60)
+        Me.Label7.Location = New System.Drawing.Point(5, 57)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(114, 13)
+        Me.Label7.Size = New System.Drawing.Size(111, 13)
         Me.Label7.TabIndex = 10
-        Me.Label7.Text = "Select Graphic Driver :"
+        Me.Label7.Text = "Select Graphic Driver:"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label8.ForeColor = System.Drawing.Color.Red
-        Me.Label8.Location = New System.Drawing.Point(12, 173)
+        Me.Label8.Location = New System.Drawing.Point(5, 439)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(595, 26)
         Me.Label8.TabIndex = 11
@@ -157,36 +160,36 @@ Partial Class Form1
         'CheckBox1
         '
         Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(343, 50)
+        Me.CheckBox1.Location = New System.Drawing.Point(208, 104)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(257, 17)
+        Me.CheckBox1.Size = New System.Drawing.Size(229, 17)
         Me.CheckBox1.TabIndex = 12
-        Me.CheckBox1.Text = "Tick to remove C:\AMD or C:\NVIDIA folders too"
+        Me.CheckBox1.Text = "Remove C:\AMD and or C:\NVIDIA folders"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
         'CheckBox2
         '
         Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(343, 70)
+        Me.CheckBox2.Location = New System.Drawing.Point(210, 127)
         Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(159, 17)
+        Me.CheckBox2.Size = New System.Drawing.Size(235, 17)
         Me.CheckBox2.TabIndex = 13
-        Me.CheckBox2.Text = "Save log files in startup path"
+        Me.CheckBox2.Text = "Save log files in startup path (recommended)"
         Me.CheckBox2.UseVisualStyleBackColor = True
         '
         'MenuStrip1
         '
         Me.MenuStrip1.Dock = System.Windows.Forms.DockStyle.Right
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1})
-        Me.MenuStrip1.Location = New System.Drawing.Point(582, 0)
+        Me.MenuStrip1.Location = New System.Drawing.Point(570, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(30, 404)
+        Me.MenuStrip1.Size = New System.Drawing.Size(30, 505)
         Me.MenuStrip1.TabIndex = 14
         Me.MenuStrip1.Text = "MenuStrip1"
         '
         'ToolStripMenuItem1
         '
-        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.ToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.VisitGuru3dNVIDIAThreadToolStripMenuItem, Me.VisitGuru3dAMDThreadToolStripMenuItem, Me.VisitGeforceThreadToolStripMenuItem})
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
         Me.ToolStripMenuItem1.Size = New System.Drawing.Size(17, 19)
         Me.ToolStripMenuItem1.Text = "?"
@@ -194,7 +197,7 @@ Partial Class Form1
         'AboutToolStripMenuItem
         '
         Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
-        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(219, 22)
         Me.AboutToolStripMenuItem.Text = "About"
         '
         'Button2
@@ -209,7 +212,7 @@ Partial Class Form1
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(411, 92)
+        Me.Button3.Location = New System.Drawing.Point(8, 244)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(195, 70)
         Me.Button3.TabIndex = 16
@@ -222,7 +225,7 @@ Partial Class Form1
         Me.CheckBox3.AutoSize = True
         Me.CheckBox3.Checked = True
         Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.CheckBox3.Location = New System.Drawing.Point(343, 30)
+        Me.CheckBox3.Location = New System.Drawing.Point(208, 81)
         Me.CheckBox3.Name = "CheckBox3"
         Me.CheckBox3.Size = New System.Drawing.Size(99, 17)
         Me.CheckBox3.TabIndex = 17
@@ -230,11 +233,39 @@ Partial Class Form1
         Me.CheckBox3.UseVisualStyleBackColor = True
         Me.CheckBox3.Visible = False
         '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(5, 478)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(325, 13)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Note: Any damage or errors caused to your PC is  your responsibility."
+        '
+        'VisitGuru3dNVIDIAThreadToolStripMenuItem
+        '
+        Me.VisitGuru3dNVIDIAThreadToolStripMenuItem.Name = "VisitGuru3dNVIDIAThreadToolStripMenuItem"
+        Me.VisitGuru3dNVIDIAThreadToolStripMenuItem.Size = New System.Drawing.Size(219, 22)
+        Me.VisitGuru3dNVIDIAThreadToolStripMenuItem.Text = "Visit Guru3d NVIDIA Thread"
+        '
+        'VisitGuru3dAMDThreadToolStripMenuItem
+        '
+        Me.VisitGuru3dAMDThreadToolStripMenuItem.Name = "VisitGuru3dAMDThreadToolStripMenuItem"
+        Me.VisitGuru3dAMDThreadToolStripMenuItem.Size = New System.Drawing.Size(219, 22)
+        Me.VisitGuru3dAMDThreadToolStripMenuItem.Text = "Visit Guru3d AMD Thread"
+        '
+        'VisitGeforceThreadToolStripMenuItem
+        '
+        Me.VisitGeforceThreadToolStripMenuItem.Name = "VisitGeforceThreadToolStripMenuItem"
+        Me.VisitGeforceThreadToolStripMenuItem.Size = New System.Drawing.Size(219, 22)
+        Me.VisitGeforceThreadToolStripMenuItem.Text = "Visit Geforce Thread"
+        '
         'PictureBox1
         '
         Me.PictureBox1.Image = Global.WindowsApplication1.My.Resources.Resources.btn_donate_LG
         Me.PictureBox1.InitialImage = CType(resources.GetObject("PictureBox1.InitialImage"), System.Drawing.Image)
-        Me.PictureBox1.Location = New System.Drawing.Point(486, 0)
+        Me.PictureBox1.Location = New System.Drawing.Point(495, 468)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(93, 28)
         Me.PictureBox1.TabIndex = 18
@@ -244,7 +275,8 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(612, 404)
+        Me.ClientSize = New System.Drawing.Size(600, 505)
+        Me.Controls.Add(Me.Label9)
         Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.CheckBox3)
         Me.Controls.Add(Me.Button3)
@@ -296,5 +328,9 @@ Partial Class Form1
     Friend WithEvents Button3 As System.Windows.Forms.Button
     Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
     Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents VisitGuru3dNVIDIAThreadToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents VisitGuru3dAMDThreadToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents VisitGeforceThreadToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
 
 End Class
