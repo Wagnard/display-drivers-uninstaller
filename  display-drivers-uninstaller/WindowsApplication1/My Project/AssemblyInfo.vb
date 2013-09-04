974ff0ec1c813562e6ff3a237a09d42976d00563
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-09-04 14:58:43 +0000

    Bump version to 7.0 for future release. Now detect windows 8.1 os app_ini_dll stuff... set to 0 if no external dll detected. (avoit event log warnings.)

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.3.0.0")> 
-<Assembly: AssemblyFileVersion("6.3.0.0")> 
+<Assembly: AssemblyVersion("7.0.0.0")> 
+<Assembly: AssemblyFileVersion("7.0.0.0")> 
