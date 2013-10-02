46d861710500807222f0799acd4344c1a78aff6a
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-10-02 15:08:31 +0000

    Release 7.8.2
    More AMD Fuel Stuff.

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("7.8.1.0")> 
-<Assembly: AssemblyFileVersion("7.8.1.0")> 
+<Assembly: AssemblyVersion("7.8.2.0")> 
+<Assembly: AssemblyFileVersion("7.8.2.0")> 
