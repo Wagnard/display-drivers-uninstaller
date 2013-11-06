9234aa35b31517aebbbbddd595b2c953fb7d36e6
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-11-06 00:53:50 +0000

    Bump to 9.3 -By popular demand, clean the others Controlsets

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("9.2.0.0")> 
-<Assembly: AssemblyFileVersion("9.2.0.0")> 
+<Assembly: AssemblyVersion("9.3.0.0")> 
+<Assembly: AssemblyFileVersion("9.3.0.0")> 
