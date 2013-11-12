9186d0b3c6bbe00b1778b78f2f8f5fb58af1d19e
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-11-12 19:08:13 +0000

    Bump to 9.6 update tos.txt

--- trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -132,7 +132,7 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(29, 13)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "V9.4"
+        Me.Label6.Text = "V9.5"
         '
         'ComboBox1
         '
