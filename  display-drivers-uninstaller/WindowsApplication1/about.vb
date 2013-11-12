8466d60d410184d2add00e794a5522215def2d95
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-11-12 17:44:27 +0000

    -replaced devcon with ddudr (made from devcon sample code) to be legal and respect microsoft license.
    added some license to our project files.

---  display-drivers-uninstaller/WindowsApplication1/about.vb
+++  display-drivers-uninstaller/WindowsApplication1/about.vb
@@ -1,4 +1,20 @@
-﻿Imports System.IO
+﻿'    Display driver Uninstaller (DDU) a driver uninstaller / Cleaner for Windows
+'    Copyright (C) <2013>  <DDU dev team>
+
+'    This program is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+
+'    This program is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+
+'    You should have received a copy of the GNU General Public License
+'    along with DDU.  If not, see <http://www.gnu.org/licenses/>.
+
+Imports System.IO
 Imports System.Text
 
 Public Class about
