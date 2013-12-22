7f3e3d508cd6d217b4e291a751a3cf57153aae42
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2013-12-22 10:21:39 +0000

    Bump version to 10.0
    -Fixed a bug withthe auto notification system. version 0 would not check correctly with the older one because we were checking <= on String variable...
    Due to this, it is necessary to upload the currentversion2.txt to the servers while still keeping the older one.
    
    Speed up DDU reg cleanup by not wasting time on changing many permission unecessarly on the pnpresources section but only on the one that we plan to delete.

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("9.9.2.0")> 
-<Assembly: AssemblyFileVersion("9.9.2.0")> 
+<Assembly: AssemblyVersion("10.0.0.0")> 
+<Assembly: AssemblyFileVersion("10.0.0.0")> 
