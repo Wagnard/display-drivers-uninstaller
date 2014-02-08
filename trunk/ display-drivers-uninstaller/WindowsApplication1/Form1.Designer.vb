36b14cfecd89917ccd1696e9353bb94d3ae38daa
wagnard28@gmail.com <wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-02-08 15:45:29 +0000

    Release 12.1 Fix a bug that would prevent the OpenCL of other vendor to work.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/Form1.Designer.vb
@@ -132,7 +132,7 @@ Partial Class Form1
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(35, 13)
         Me.Label6.TabIndex = 8
-        Me.Label6.Text = "V12.0"
+        Me.Label6.Text = "V12.1"
         '
         'ComboBox1
         '
