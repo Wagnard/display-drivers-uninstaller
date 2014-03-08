c3564d1292436342375d840c839955d88955eaaf
wagnard28@gmail.com <wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-03-08 14:22:55 +0000

    Bump Version to 12.4.1 Forgot to put a try/catch for the deletion of the files in the prefetch folder. (fixed a crash if prefetch doesn't exist for some reason)

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("12.4.0.0")> 
-<Assembly: AssemblyFileVersion("12.4.0.0")> 
+<Assembly: AssemblyVersion("12.4.1.0")> 
+<Assembly: AssemblyFileVersion("12.4.1.0")> 
