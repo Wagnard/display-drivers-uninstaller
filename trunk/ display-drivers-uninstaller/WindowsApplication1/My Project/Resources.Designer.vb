9299269bce243e8fb25549bed243653f9cae8d6c
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-07-27 13:49:41 +0000

    Release 12.9.8.0 Extract the paypal.bat instead of just including it in the settings folder. This is to help prevent someone modifying the .bat and giving donation to the wrong guy.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/Resources.Designer.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/Resources.Designer.vb
@@ -163,6 +163,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Byte[].
         '''</summary>
+        Friend ReadOnly Property paypal() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("paypal", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Byte[].
+        '''</summary>
         Friend ReadOnly Property pnpldfwin7vista() As Byte()
             Get
                 Dim obj As Object = ResourceManager.GetObject("pnpldfwin7vista", resourceCulture)
