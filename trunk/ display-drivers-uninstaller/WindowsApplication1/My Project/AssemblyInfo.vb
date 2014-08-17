3491f186bbde7b72805c4d2786ca729e48436cc4
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-08-17 00:58:32 +0000

    Release 12.9.9.2 -Fixed the removal of disconnected / non present devices.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("12.9.9.1")> 
-<Assembly: AssemblyFileVersion("12.9.9.1")> 
+<Assembly: AssemblyVersion("12.9.9.2")> 
+<Assembly: AssemblyFileVersion("12.9.9.2")> 
