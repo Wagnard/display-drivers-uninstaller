220112c05b6dd0308d4cd9c1d6fa464a4776c7ff
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-08-19 12:35:24 +0000

    Bump to V13.0.0.0 Translation updates : Chinese (Simplified & Traditional), Dutch, Serbian (Cyrilic & Latin)

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("12.9.9.3")> 
-<Assembly: AssemblyFileVersion("12.9.9.3")> 
+<Assembly: AssemblyVersion("13.0.0.0")> 
+<Assembly: AssemblyFileVersion("13.0.0.0")> 
