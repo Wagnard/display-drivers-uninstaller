49e3a6085d2e50f42d75119b9383ed19912b696c
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-09-11 13:47:06 +0000

    Release 13.1.0.0

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("13.0.1.1")> 
-<Assembly: AssemblyFileVersion("13.0.1.1")> 
+<Assembly: AssemblyVersion("13.1.0.0")> 
+<Assembly: AssemblyFileVersion("13.1.0.0")> 
