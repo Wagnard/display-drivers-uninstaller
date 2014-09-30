c7213bf503f13390b12441fdc5a010159a273dee
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-09-30 19:21:24 +0000

    Additional AMD Cleanup. (ACP Application)  Thanks for MojoW help on this.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("13.3.0.1")> 
-<Assembly: AssemblyFileVersion("13.3.0.1")> 
+<Assembly: AssemblyVersion("13.3.0.2")> 
+<Assembly: AssemblyFileVersion("13.3.0.2")> 
