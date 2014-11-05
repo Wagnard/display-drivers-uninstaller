5c39f89b1f6bc05c6da97026b8b6c01bed2a0925
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-11-05 14:52:30 +0000

    Real 13.5.1.1 Release. Prevent resizing of the option form. minor nV prefetch driver file cleanUP.

---  display-drivers-uninstaller/WindowsApplication1/options.Designer.vb
+++  display-drivers-uninstaller/WindowsApplication1/options.Designer.vb
@@ -105,7 +105,9 @@ Partial Class options
         Me.Controls.Add(Me.CheckBox3)
         Me.Controls.Add(Me.CheckBox2)
         Me.Controls.Add(Me.CheckBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "options"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Options"
