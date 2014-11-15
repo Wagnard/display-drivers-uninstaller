3262bf5d735e1feff6b49a94d328f4c062f411c5
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-11-15 16:24:06 +0000

    Release 13.5.1.3 Updated Chinese (Traditional) Thanks to Danfong Hsieh Several fixes / enhancement.

---  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++  display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("13.5.1.2")> 
-<Assembly: AssemblyFileVersion("13.5.1.2")> 
+<Assembly: AssemblyVersion("13.5.1.3")> 
+<Assembly: AssemblyFileVersion("13.5.1.3")> 
