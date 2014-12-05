a34c863017c7e6ef590a5c977cffe53e00c7f245
Wagnard28@gmail.com <Wagnard28@gmail.com@0a27b0de-173e-4de1-8a09-80ae67cedccf>
2014-12-05 16:58:30 +0000

    Release 13.5.3.1 -Fix some possibles DDU freeze when cleaning. -Japanese language update. (DH.Omega)
    p.s The cfg were supposed to be on the earlier svn commit. I forgot them. But they were included in the release so no biggies.

--- trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
+++ trunk/ display-drivers-uninstaller/WindowsApplication1/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("13.5.3.0")> 
-<Assembly: AssemblyFileVersion("13.5.3.0")> 
+<Assembly: AssemblyVersion("13.5.3.1")> 
+<Assembly: AssemblyFileVersion("13.5.3.1")> 
