a404e0d97ec17930056b435c1a1183647ed4d83b
Wagnard <wagnard28@gmail.com>
2015-06-27 10:35:30 -0400

    Enabled the checkbox9 name translation for the option form.

---  display-drivers-uninstaller/WindowsApplication1/options.vb
+++  display-drivers-uninstaller/WindowsApplication1/options.vb
@@ -216,6 +216,14 @@ Public Class options
             CheckBox8.Text = CheckBox8.Text & buttontext(i)
         Next
 
+        buttontext = IO.File.ReadAllLines(Application.StartupPath & "\settings\Languages\" & combobox2value & "\checkbox9.txt") '// add each line as String Array.
+        CheckBox9.Text = ""
+        For i As Integer = 0 To buttontext.Length - 1
+            If i <> 0 Then
+                CheckBox9.Text = CheckBox9.Text
+            End If
+            CheckBox9.Text = CheckBox9.Text & buttontext(i)
+        Next
 
 
     End Sub
