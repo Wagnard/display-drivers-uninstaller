6c3287fc5b2fb3aaa6c4c4bbd75c5192a2ce7c38
Wagnard <wagnard28@gmail.com>
2016-03-11 11:47:04 -0500

    Renamed button1 to btnClose in frmLog Added the appropriate changes to the languages xml files including the integrated English.xml to allow its translation.

--- display-drivers-uninstaller/WindowsApplication1/frmLog.Designer.vb
+++ display-drivers-uninstaller/WindowsApplication1/frmLog.Designer.vb
@@ -22,48 +22,48 @@ Partial Class frmLog
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()> _
 	Private Sub InitializeComponent()
-		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLog))
-		Me.TextBox1 = New System.Windows.Forms.TextBox()
-		Me.Button1 = New System.Windows.Forms.Button()
-		Me.SuspendLayout()
-		'
-		'TextBox1
-		'
-		Me.TextBox1.BackColor = System.Drawing.SystemColors.MenuBar
-		Me.TextBox1.Location = New System.Drawing.Point(8, 8)
-		Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
-		Me.TextBox1.Multiline = True
-		Me.TextBox1.Name = "TextBox1"
-		Me.TextBox1.ReadOnly = True
-		Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-		Me.TextBox1.Size = New System.Drawing.Size(581, 428)
-		Me.TextBox1.TabIndex = 0
-		'
-		'Button1
-		'
-		Me.Button1.Location = New System.Drawing.Point(509, 441)
-		Me.Button1.Name = "Button1"
-		Me.Button1.Size = New System.Drawing.Size(75, 23)
-		Me.Button1.TabIndex = 43
-		Me.Button1.Text = "Close"
-		Me.Button1.UseVisualStyleBackColor = True
-		'
-		'logf
-		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(596, 476)
-		Me.Controls.Add(Me.Button1)
-		Me.Controls.Add(Me.TextBox1)
-		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-		Me.KeyPreview = True
-		Me.Margin = New System.Windows.Forms.Padding(2)
-		Me.Name = "frmLog"
-		Me.Text = "Extended Log"
-		Me.ResumeLayout(False)
-		Me.PerformLayout()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmLog))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.SystemColors.MenuBar
+        Me.TextBox1.Location = New System.Drawing.Point(8, 8)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(581, 428)
+        Me.TextBox1.TabIndex = 0
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(509, 441)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 43
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'frmLog
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(596, 476)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "frmLog"
+        Me.Text = "Extended Log"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-	End Sub
-	Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
-	Friend WithEvents Button1 As System.Windows.Forms.Button
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnClose As System.Windows.Forms.Button
 End Class
