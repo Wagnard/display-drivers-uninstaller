ab3b8dc9c4f098d4e614010ceb735f54787e50fc
Wagnard <wagnard28@gmail.com>
2016-03-24 10:03:17 -0400

    Small translations file update (French/Finish/English)

--- display-driver-uninstaller/Display Driver Uninstaller/frmOptions.xaml.vb
+++ display-driver-uninstaller/Display Driver Uninstaller/frmOptions.xaml.vb
@@ -3,4 +3,8 @@
 	Private Sub frmOptions_Loaded(sender As System.Object, e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
 		Languages.TranslateForm(Me)
 	End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As RoutedEventArgs) Handles btnClose.Click
+        Me.Close()
+    End Sub
 End Class
