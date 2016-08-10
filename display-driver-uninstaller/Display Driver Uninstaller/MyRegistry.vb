aabce20c9aef85f34a2cc899db48bb8adb67e2ff
Wagnard <wagnard28@gmail.com>
2016-08-10 19:35:09 -0400

    Small cleanup on my last commits.

--- display-driver-uninstaller/Display Driver Uninstaller/MyRegistry.vb
+++ display-driver-uninstaller/Display Driver Uninstaller/MyRegistry.vb
@@ -4,7 +4,7 @@ Imports Microsoft.Win32
 
 
 Public Class MyRegistry
-	Implements IDisposable
+
 
 	Public Shared Function OpenSubKey(RootKey As RegistryKey, Key As String, Optional Writable As Boolean = False) As RegistryKey
 		Dim FixPerm As Boolean = False
@@ -29,37 +29,5 @@ Public Class MyRegistry
 			Return RootKey.OpenSubKey(Key, Writable)
 		End Try
 	End Function
-
-#Region "IDisposable Support"
-	Private disposedValue As Boolean ' To detect redundant calls
-
-	' IDisposable
-	Protected Overridable Sub Dispose(disposing As Boolean)
-		If Not Me.disposedValue Then
-			If disposing Then
-				' TODO: dispose managed state (managed objects).
-
-			End If
-
-			' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
-			' TODO: set large fields to null.
-		End If
-		Me.disposedValue = True
-	End Sub
-
-	' TODO: override Finalize() only if Dispose(ByVal disposing As Boolean) above has code to free unmanaged resources.
-	'Protected Overrides Sub Finalize()
-	'    ' Do not change this code.  Put cleanup code in Dispose(ByVal disposing As Boolean) above.
-	'    Dispose(False)
-	'    MyBase.Finalize()
-	'End Sub
-
-	' This code added by Visual Basic to correctly implement the disposable pattern.
-	Public Sub Dispose() Implements IDisposable.Dispose
-		' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
-		Dispose(True)
-		GC.SuppressFinalize(Me)
-	End Sub
-#End Region
 
 End Class
