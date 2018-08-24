e5edad3d152c7c66e1336df6e0e61a08d77e78b5
Ghislain Harvey <wagnard28@gmail.com>
2018-08-24 16:49:41 -0400

    Aditional Realtek stuff.

--- display-driver-uninstaller/Display Driver Uninstaller/AUDIOCleanup.vb
+++ display-driver-uninstaller/Display Driver Uninstaller/AUDIOCleanup.vb
@@ -73,7 +73,7 @@ Public Class AUDIOCleanup
 	Private Sub CleanRealtekserviceprocess()
 		Application.Log.AddMessage("Cleaning Process/Services...")
 
-		CleanupEngine.Cleanserviceprocess(IO.File.ReadAllLines(Application.Paths.AppBase & "settings\REALTEK\services.cfg"))    '// add each line as String Array.
+		CleanupEngine.Cleanserviceprocess(IO.File.ReadAllLines(Application.Paths.AppBase & "settings\REALTEK\services.cfg"))
 
 		KillProcess("RtkNGUI64")
 		Application.Log.AddMessage("Process/Services CleanUP Complete")
@@ -91,6 +91,65 @@ Public Class AUDIOCleanup
 	End Sub
 
 	Private Sub CleanRealtekFolders(ByVal config As ThreadSettings)
+		Dim filePath As String = Nothing
+		UpdateTextMethod(UpdateTextTranslated(1))
+
+		Application.Log.AddMessage("Cleaning Directories (Please Wait...)")
+
+		filePath = config.Paths.ProgramFiles + "Realtek"
+		If FileIO.ExistsDir(filePath) Then
+
+			For Each child As String In FileIO.GetDirectories(filePath)
+				If IsNullOrWhitespace(child) = False Then
+					If StrContainsAny(child, True, "audio") Then
+
+						Delete(child)
+
+					End If
+				End If
+			Next
+			If FileIO.CountDirectories(filePath) = 0 Then
+
+				Delete(filePath)
+
+			Else
+				For Each data As String In FileIO.GetDirectories(filePath)
+					If IsNullOrWhitespace(data) Then Continue For
+					Application.Log.AddWarningMessage("Remaining folders found " + " : " + filePath + "\ --> " + data)
+				Next
+			End If
+		End If
+
+
+		'64Bit zone
+		If IntPtr.Size = 8 Then
+			filePath = config.Paths.ProgramFilesx86 + "Realtek"
+			If FileIO.ExistsDir(filePath) Then
+				Try
+					For Each child As String In FileIO.GetDirectories(filePath)
+						If IsNullOrWhitespace(child) = False Then
+							If StrContainsAny(child, True, "Audio") Then
+
+								Delete(child)
+
+							End If
+						End If
+					Next
+					If FileIO.CountDirectories(filePath) = 0 Then
+
+						Delete(filePath)
+
+					Else
+						For Each data As String In FileIO.GetDirectories(filePath)
+							If IsNullOrWhitespace(data) Then Continue For
+							Application.Log.AddWarningMessage("Remaining folders found " + " : " + filePath + "\ --> " + data)
+						Next
+
+					End If
+				Catch ex As Exception
+				End Try
+			End If
+		End If
 
 	End Sub
 
@@ -117,5 +176,10 @@ Public Class AUDIOCleanup
 	Private Function UpdateTextTranslated(ByVal number As Integer) As String
 		Return frmMain.UpdateTextTranslated(number)
 	End Function
+
+	Private Sub Delete(ByVal filename As String)
+		FileIO.Delete(filename)
+		CleanupEngine.RemoveSharedDlls(filename)
+	End Sub
 
 End Class
