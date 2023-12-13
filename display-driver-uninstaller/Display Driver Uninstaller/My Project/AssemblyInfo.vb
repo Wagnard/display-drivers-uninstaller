fd077fd266c9f16132a37cc0456139ea545b7044
Wagnard <waganrd28@gmail.com>
2023-12-13 12:31:44 -0500

    Translation: Slovenian.xml

--- display-driver-uninstaller/Display Driver Uninstaller/My Project/AssemblyInfo.vb
+++ display-driver-uninstaller/Display Driver Uninstaller/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("18.0.6.9")>
-<Assembly: AssemblyFileVersion("18.0.6.9")>
+<Assembly: AssemblyVersion("18.0.7.0")>
+<Assembly: AssemblyFileVersion("18.0.7.0")>
