0b908eb3ab6927afb7d152d4709420784ec2c4c2
Wagnard <wagnard28@gmail.com>
2026-09-13 14:05:02 -0400

    Fixed : kept OEM extensions were still removed by the driver store cleanup (Intel, Qualcomm)

--- display-driver-uninstaller/Display Driver Uninstaller/CleanupEngine.vb
+++ display-driver-uninstaller/Display Driver Uninstaller/CleanupEngine.vb
@@ -3650,17 +3650,17 @@ Namespace Display_Driver_Uninstaller
 
                     If StrContainsAny(oem.Class, True, "display", "media", "extension", "softwarecomponent", "CTA Driver Devices", "system", "SoftwareDevice", "USB") Then
                         'OEM extensions are kept unless asked otherwise. Intel's own extensions (audio bus, firmware update...) still go.
-                        If config.SelectedType = CleanType.GPU AndAlso StrContainsAny(oem.Class, True, "Extension") Then
-                            If (config.SelectedGPU = GPUVendor.Intel AndAlso Not config.RemoveIntelExt AndAlso Not StrContainsAny(oem.Catalog, True, "igdlh.cat", "HdBusExt.cat", "IntelGFXFwUpdate.cat", "Intel_NF_I2C.cat")) OrElse
-                               (config.SelectedGPU = GPUVendor.Qualcomm AndAlso Not config.RemoveQualcommExt) Then
-                                Application.Log.AddMessage("Extension INF kept: " & oem.FileName)
-                                Continue For
-                            End If
-                        End If
+                        Dim keepOemExtension As Boolean = config.SelectedType = CleanType.GPU AndAlso StrContainsAny(oem.Class, True, "Extension") AndAlso
+                            ((config.SelectedGPU = GPUVendor.Intel AndAlso Not config.RemoveIntelExt AndAlso Not StrContainsAny(oem.Catalog, True, "igdlh.cat", "HdBusExt.cat", "IntelGFXFwUpdate.cat", "Intel_NF_I2C.cat")) OrElse
+                             (config.SelectedGPU = GPUVendor.Qualcomm AndAlso Not config.RemoveQualcommExt))
 
                         If Not ((Not config.RemoveNVBROADCAST AndAlso StrContainsAny(oem.Catalog, True, "nvrtxvad")) Or (Not config.RemoveGFE AndAlso StrContainsAny(oem.Catalog, True, "nvvad")) Or (Not config.RemoveGFE AndAlso StrContainsAny(oem.Catalog, True, "nvswcfilter")) Or ((Not config.RemoveAMDKMPFD Or Not config.NotPresentAMDKMPFD) AndAlso StrContainsAny(oem.Catalog, True, "amdkmpfd"))) Then
                             If StrContainsAny(oem.Class, True, "Extension") AndAlso StrContainsAny(oem.Catalog, True, "extinf.cat", "amdpcibridgeextension.cat", "igdlh.cat") Then
-                                SetupAPI.RemoveInf(oem, False)
+                                If keepOemExtension Then
+                                    Application.Log.AddMessage("Extension INF kept: " & oem.FileName)
+                                Else
+                                    SetupAPI.RemoveInf(oem, False)
+                                End If
                                 Continue For
                             End If
 
@@ -3776,7 +3776,11 @@ Namespace Display_Driver_Uninstaller
                             For Each SourceDisksName In oem.SourceDisksFiles
                                 If String.IsNullOrWhiteSpace(SourceDisksName) Then Continue For
                                 If SourceDisksName IsNot Nothing AndAlso StrContainsAny(SourceDisksName, True, driverfiles) Then
-                                    SetupAPI.RemoveInf(oem, False)
+                                    If keepOemExtension Then
+                                        Application.Log.AddMessage("Extension INF kept: " & oem.FileName)
+                                    Else
+                                        SetupAPI.RemoveInf(oem, False)
+                                    End If
                                     Exit For
                                 End If
                             Next
